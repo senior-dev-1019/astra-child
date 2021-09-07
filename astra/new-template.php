a1e7ac8105cf87a8fc39a0820e5378bbd8dcf4d4
senior-developer1019 <romo1112@yandex.com>
2021-09-08 00:11:53 +0900

    Initial commit

---
+++ astra/new-template.php
@@ -0,0 +1,25 @@
+<?php 
+
+if ( astra_page_layout() == 'left-sidebar' ) : ?>
+
+	<?php get_sidebar(); ?>
+
+<?php endif ?>
+
+	<div id="primary" <?php astra_primary_class(); ?>>
+
+		<?php astra_primary_content_top(); ?>
+
+		<?php astra_content_loop(); ?>
+
+		<?php astra_primary_content_bottom(); ?>
+
+	</div><!-- #primary -->
+
+<?php if ( astra_page_layout() == 'right-sidebar' ) : ?>
+
+	<?php get_sidebar(); ?>
+
+<?php endif ?>
+
+<?php get_footer(); ?>
