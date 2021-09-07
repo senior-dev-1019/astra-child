8309e8e1b7e6534214b3c9601f12d8cff84d69ad
senior-developer1019 <romo1112@yandex.com>
2021-09-08 00:40:22 +0900

    create regular-page template

---
+++ astra/regular-page.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+ * Template Name: Regular Page
+ * description: >-
+  Regular Page template as new regular design 
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header(); ?>
+
+<?php if ( astra_page_layout() == 'left-sidebar' ) : ?>
+
+	<?php get_sidebar(); ?>
+
+<?php endif ?>
+
+	<div id="primary" <?php astra_primary_class(); ?>>
+
+		<?php astra_primary_content_top(); ?>
+
+		<?php astra_content_page_loop(); ?>
+
+		<?php astra_primary_content_bottom(); ?>
+
+	</div><!-- #primary -->
+
+<?php if ( astra_page_layout() == 'right-sidebar' ) : ?>
+
+	<?php get_sidebar(); ?>
+
+<?php endif ?>
+
+<?php get_footer(); ?>
