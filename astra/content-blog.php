7842778858d8cde976874fa4c2cc44c372e1b951
senior-developer1019 <romo1112@yandex.com>
2021-09-21 06:20:55 +0900

    add news template

--- astra/content-blog.php
+++ astra/content-blog.php
@@ -10,7 +10,17 @@
 
 ?>
 <?php astra_entry_before(); ?>
-<article
+<style type="text/css">
+	#primary {
+		margin:  0 !important;
+	}
+</style>
+<header class="entry-header" style='background-image: url(<?php echo get_stylesheet_directory_uri() ?>/assets/images/news.jpg); text-align: center;'>
+	<div class="astra-child-title">
+		<h2 class="entry-child-title child_title_news">What's the news!</h2>
+	</div>
+</header>
+<article style="background-color: #91FF30;"
 <?php
 		echo astra_attr(
 			'article-blog',
